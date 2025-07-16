8e3decc0b3f3f7bd89e67b467dd97998252f1a67
kylelanc86 <kylelanc86@gmail.com>
2025-07-16 12:26:03 +1000

    Temporary fixes: add canvg dependency to frontend, remove xero dependency from backend to resolve build timeouts

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const jobRoutes = require('./routes/jobs');
 const sampleRoutes = require('./routes/samples');
 const invoiceRoutes = require('./routes/invoices');
 const usersRouter = require('./routes/users');
-const xeroRoutes = require('./routes/xero');
+// const xeroRoutes = require('./routes/xero');
 const shiftRoutes = require('./routes/shifts');
 const timesheetRoutes = require('./routes/timesheets');
 const calendarEntriesRouter = require('./routes/calendarEntries');
@@ -109,7 +109,7 @@ connectDB()
     app.use('/api/samples', sampleRoutes);
     app.use('/api/invoices', invoiceRoutes);
     app.use('/api/users', usersRouter);
-    app.use('/api/xero', xeroRoutes);
+    // app.use('/api/xero', xeroRoutes);
     app.use('/api/air-monitoring-shifts', shiftRoutes);
     app.use('/api/timesheets', timesheetRoutes);
     app.use('/api/calendar-entries', calendarEntriesRouter);
