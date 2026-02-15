5cc938a55b6a2596706d2f1313f40f12b5ead431
kylelanc86 <kylelanc86@gmail.com>
2026-02-15 19:40:32 +1100

    not sure!

--- frontend/src/scenes/records/audits.jsx
+++ frontend/src/scenes/records/audits.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import { Box, Typography, Paper, Breadcrumbs, Link } from "@mui/material";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { ArrowBack as ArrowBackIcon } from "@mui/icons-material";
